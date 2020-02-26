8d8c7c646e0c64202f76145662c06aa05e69a092
gortron <gordon.lanza@gmail.com>
2020-02-26 15:01:44 -0800

    solved cli exercise by using readFileSync() and writeFileSync() methods

--- exercises/cli/utils.js
+++ exercises/cli/utils.js
@@ -1,8 +1,8 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
 // this path needs to be relative to work with fs
-const contactsLocation = 'contacts.json'
+const contactsLocation = "./exercises/cli/contacts.json";
 
 /**
  * should read the contacts at the
@@ -10,21 +10,23 @@ const contactsLocation = 'contacts.json'
  * it to a js object
  */
 const getContacts = () => {
-  
-}
+  let contactsString = fs.readFileSync(contactsLocation);
+  let contacts = JSON.parse(contactsString);
+  return contacts;
+};
 
 /**
  * takes a contacts object, converts it to JSON
  * and saves it at the @contactsLocation path
  * @param {Object} contacts contacts object
  */
-const saveContacts = (contacts) => {
-
-}
+const saveContacts = contacts => {
+  let contactsString = JSON.stringify(contacts);
+  fs.writeFileSync(contactsLocation, contactsString);
+};
 
 module.exports = {
   contactsLocation,
   getContacts,
   saveContacts
-}
-
+};
