322c0fb0dd50c2e2211f0ba5ee38408cb1a128f6
gortron <gordon.lanza@gmail.com>
2020-02-27 09:43:04 -0800

    completed testing exercise by debugging node files and adding jest tests

--- exercises/testing/test.js
+++ exercises/testing/test.js
@@ -1,4 +1,25 @@
-// write some tests
-describe('users', () => {
-  
-})
+const { findUser, deleteUser } = require("./users");
+
+describe("users", () => {
+  describe("findUser", () => {
+    test("finds user by id if user is there", async () => {
+      const user = await findUser(1);
+      expect(user.id).toBe(1);
+    });
+  });
+
+  describe("deletUser", () => {
+    test("deletes user with id if user is there", async () => {
+      expect.assertions(1);
+
+      const user = await findUser(1);
+      const deletedUser = await deleteUser(user.id);
+      expect(deletedUser.id).toBe(1);
+      try {
+        await findUser(1);
+      } catch (e) {
+        expect(e).toBeTruthy();
+      }
+    });
+  });
+});
